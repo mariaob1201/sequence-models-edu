f9accd9a45e75fa02cebb5e8dee7fd6e5eccdc58
Maria Oros <mariaoros@Marias-MacBook-Pro.local>
2025-02-16 17:36:34 -0600

    ADD notebooks

---
+++ rnn.py
@@ -0,0 +1,44 @@
+def optimize(X, Y, a_prev, parameters, learning_rate=0.01):
+    """
+    Execute one step of the optimization to train the model.
+
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+
+    ### START CODE HERE ###
+
+    # Forward propagate through time
+    loss, cache = rnn_forward(X, Y, a_prev, parameters)
+
+    # Backpropagate through time
+    gradients, a = rnn_backward(X, Y, cache, parameters)
+
+    # Clip gradients to mitigate exploding gradients
+    gradients = clip(gradients, 5)  # clip between -5 and 5
+
+    # Update parameters using gradient descent
+    parameters = update_parameters(parameters, gradients, learning_rate)
+
+    ### END CODE HERE ###
+
+    return loss, gradients, a[len(X) - 1]
